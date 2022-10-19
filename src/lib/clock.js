83cbbbcb1ece0ed53b8fafd8105a7a6f306b483f
Josh Nussbaum <joshnuss@gmail.com>
2022-10-19 13:51:42 -0400

    Fix clock cleanup logic

--- src/lib/clock.js
+++ src/lib/clock.js
@@ -4,5 +4,5 @@ export const now = readable(new Date(), (set) => {
   const update = () => set(new Date())
   const interval = setInterval(update, 60000)
 
-  return clearInterval(interval)
+  return () => clearInterval(interval)
 })
